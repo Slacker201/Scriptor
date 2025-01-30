721e844f7215088ca7b3d74b1824af3158c3c1c2
Slacker201 <slacker1234567809@gmail.com>
2025-01-30 12:37:07 -0600

    Added chat mixin

--- src/client/java/com/slacker201/mixin/client/ChatScreenMixin.java
+++ src/client/java/com/slacker201/mixin/client/ChatScreenMixin.java
@@ -2,15 +2,8 @@ package com.slacker201.mixin.client;
 
 
 import net.minecraft.client.gui.screen.ChatScreen;
-
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
-import net.minecraft.util.StringHelper;
-import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
